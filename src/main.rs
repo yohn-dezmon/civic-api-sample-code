ab0d9b4ebe7e3438a18f0a68c798e2a4da8fa088
John Desmond <johndesmond631@gmail.com>
2023-03-04 11:43:57 -0500

    not yet working, hyper may not accept https?

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use hyper::{Client, Uri};
+use std::str::FromStr;
+
+// tokio runtime for async code
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    // using &str because I want to be able to reuse `url` and I won't need to 
+    // modify it.
+    let host_name: &str = "https://api.open.fec.gov/v1";
+    let endpoint: &str = "/candidate/1";
+
+    let candidate_1_endpoint = format!("{}{}", host_name, endpoint);
+    let c_1_ref = candidate_1_endpoint.as_str();
+    
+    let http_client = Client::new();
+    // why is there a question mark before the semi-colon?
+    let endpoint_uri = Uri::from_str(c_1_ref)?;
+    let response = http_client.get(endpoint_uri).await?;
+    let response_body = hyper::body::to_bytes(response.into_body()).await?;
+
+    let response_string = String::from_utf8_lossy(&response_body);
+    println!("{}", response_string);
+
+    Ok(())
 }
