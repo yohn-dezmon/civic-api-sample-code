30495ee58aa4cfffddd88b2a0ff2a781ee632e62
John Desmond <johndesmond631@gmail.com>
2023-03-05 14:34:26 -0500

    finally got a working endpoint call from OpenFEC

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,18 @@
-use hyper::{Client, Uri};
-use std::str::FromStr;
+use std::error::Error;
+use reqwest::Client;
+use std::env;
 
 // tokio runtime for async code
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // using &str because I want to be able to reuse `url` and I won't need to 
-    // modify it.
-    let host_name: &str = "https://api.open.fec.gov/v1";
-    let endpoint: &str = "/candidate/1";
-
-    let candidate_1_endpoint = format!("{}{}", host_name, endpoint);
-    let c_1_ref = candidate_1_endpoint.as_str();
-    
-    let http_client = Client::new();
-    // why is there a question mark before the semi-colon?
-    let endpoint_uri = Uri::from_str(c_1_ref)?;
-    let response = http_client.get(endpoint_uri).await?;
-    let response_body = hyper::body::to_bytes(response.into_body()).await?;
-
-    let response_string = String::from_utf8_lossy(&response_body);
-    println!("{}", response_string);
-
+async fn main() -> Result<(), Box<dyn Error>> {
+    // TODO: Make this an environment variable
+    let api_key = env::var("OPEN_FEC_API_KEY").unwrap();
+    // Daniel Goldman's candidate_id
+    let candidate_id = "H2NY10308";
+    let client = Client::new();
+    let url = format!("https://api.open.fec.gov/v1/candidate/{}?api_key={}", candidate_id, api_key);
+    let response = client.get(&url).send().await?;
+    let body = response.text().await?;
+    println!("{}", body);
     Ok(())
 }
